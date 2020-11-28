2c8415c570de3fd497dbd54ea8c1549212184b49
Ben <ben.jlawrence@hotmail.com>
2020-11-28 01:17:47 -0700

    second commit

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-$('#submitButton').on('click', function() {
-    $('#chatArea').prepend(`
-    <div id='comment'>
-        <div id='profilePicture'><img src='Image/userImage.png' alt='Profile Picture' width='100' height='100'></div>
-        <div id='input'>
-            <div id='nameDiv'>
-                ${$(displayName).val()}
-            </div>
-            <div id='commentdiv'>
-                ${$(commentArea).val()}
-            </div>
-        </div>
-        <div id='change'>
-            <div id='edit'><p>Edit</p></div>
-            <div id='remove'><p>Remove</p></div>
-        </div>
-    </div>
-    `)
-})
+$("#submitButton").on("click", function () {
+    let display = $("#displayName").val();
+    let content = $("#commentArea").val();
+    $("#chatArea").prepend(`
+      <div class = 'addedstuff' id='comment'>
+          <div class= 'addedstuff' id='profilePicture'><img src='Image/userImage.png' alt='Profile Picture' width='100' height='100'></div>
+              <div class = 'addedstuff' id='input'>
+                  <p class = 'addedstuff' id='nameDiv'>
+                      <strong>${display}</strong>
+                  </p>
+                  <p class = 'commentDiv'>
+                      ${content}
+                  </p>
+                  <div class="hiddenEdit hidden">
+                      <div class = 'addedstuff' id='hiddenComment'>
+                      <input type="text" class = 'addedstuff' id="hiddenCommentArea" placeholder="Comment" name="comment">
+                      </div>
+                      <div class = 'addedstuff' id="hiddenSubmitButton">
+                      <p>Submit</p>
+                      </div>
+                  </div>
+              </div>
+              <div class = 'addedstuff' id='change'>
+                  <div class = 'addedstuff edit'><p>Edit</p></div>
+                      
+                  <div class = 'addedstuff' id='remove'><p>Remove</p></div>
+                  
+              </div>
+      </div>
+      `);
+  });
+  
+  $("#chatArea").on("click", "#remove", function () {
+    let i = $(this).parent().parent();
+    $(i).remove();
+  });
+  
+  $("#chatArea").on("click", ".edit", function () {
+    let i = $(this).parent().parent().find(".hiddenEdit");
+    $(i).toggleClass("hidden");
+    $("#hiddenCommentArea").focus();
+  });
+  
+  $("#chatArea").on("click", "#hiddenSubmitButton", function () {
+    let i = $(this).parent().parent().parent();
+    i = $(i).children();
+    i = i.children(".commentDiv");
+    let newText = $("#hiddenCommentArea").val();
+    $(i).text(newText);
+  
+    $(this).parent().toggleClass("hidden");
+    $("#hiddenCommentArea").val("");
+  });
+  
